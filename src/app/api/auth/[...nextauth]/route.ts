ea453fcb5c74d4cf397563b412e390b104541053
Gustave Montana <77447364+gnonk323@users.noreply.github.com>
2025-02-17 21:42:15 -0500

    vercel build changes

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 
-export const handler = NextAuth({
+const handler = NextAuth({
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID!,
